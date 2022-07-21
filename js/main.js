b789f35fdb6a74784b1f0fc643f71c518d2f091a
cassio.cardoso <cassio.cardoso@sequor.com.br>
2022-07-21 00:08:15 -0300

    fim aula 3 - interagindo com os dados da web

--- js/main.js
+++ js/main.js
@@ -1,6 +1,10 @@
 const form = document.getElementById('novoItem');
 const lista = document.getElementById('lista');
-const itens = [];
+const itens = JSON.parse(localStorage.getItem('itens')) || [];
+
+itens.forEach((elemento) => {
+    criarElemento(elemento);
+})
 
 form.addEventListener('submit', (evento) => {
     evento.preventDefault();
@@ -8,30 +12,31 @@ form.addEventListener('submit', (evento) => {
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
 
-    criarElemento(nome.value, quantidade.value);
+    const itemAtual = {
+        'nome': nome.value,
+        'quantidade': quantidade.value
+    }
+
+    criarElemento(itemAtual);
+
+    itens.push(itemAtual);
+
+    localStorage.setItem('itens', JSON.stringify(itens));
 
     nome.value = '';
     quantidade.value = '';
 })
 
-function criarElemento(nome, quantidade){
+function criarElemento(item){
     const novoItem = document.createElement('li');
     novoItem.classList.add('item');
 
     const numeroItem = document.createElement('strong');
-    numeroItem.innerHTML = quantidade;
 
+    numeroItem.innerHTML = item.quantidade;
     novoItem.appendChild(numeroItem);
-    novoItem.innerHTML += nome;
 
+    novoItem.innerHTML += item.nome;
     lista.appendChild(novoItem);
 
-    const itemAtual = {
-        'nome': nome,
-        'quantidade': quantidade
-    }
-
-    itens.push(itemAtual);
-    
-    localStorage.setItem('itens', JSON.stringify(itens));
 }
