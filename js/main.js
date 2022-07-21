a5487487c3f642b5c55abffeccd306656a1cf7be
cassio.cardoso <cassio.cardoso@sequor.com.br>
2022-07-21 00:42:46 -0300

    fim 04 - atualizando um item

--- js/main.js
+++ js/main.js
@@ -12,14 +12,25 @@ form.addEventListener('submit', (evento) => {
     const nome = evento.target.elements['nome'];
     const quantidade = evento.target.elements['quantidade'];
 
+    const existe = itens.find(elemento => elemento.nome === nome.value)
+
     const itemAtual = {
         'nome': nome.value,
         'quantidade': quantidade.value
     }
 
-    criarElemento(itemAtual);
+    if (existe) {
+        itemAtual.id = existe.id;
+        atualizaElemento(itemAtual);
+        itens[existe.id] = itemAtual;
+    }   
+    else {
+        itemAtual.id = itens.length;
 
-    itens.push(itemAtual);
+        criarElemento(itemAtual);
+
+        itens.push(itemAtual);
+    }
 
     localStorage.setItem('itens', JSON.stringify(itens));
 
@@ -27,16 +38,20 @@ form.addEventListener('submit', (evento) => {
     quantidade.value = '';
 })
 
-function criarElemento(item){
+function criarElemento(item) {
     const novoItem = document.createElement('li');
     novoItem.classList.add('item');
 
     const numeroItem = document.createElement('strong');
-
     numeroItem.innerHTML = item.quantidade;
+    numeroItem.dataset.id = item.id;
     novoItem.appendChild(numeroItem);
 
     novoItem.innerHTML += item.nome;
     lista.appendChild(novoItem);
 
+}
+
+function atualizaElemento(item) {
+    document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade;
 }
